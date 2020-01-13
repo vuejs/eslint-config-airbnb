1f2400bcfdf0b5bae86a3572761928bab4f17e1f
Haoqun Jiang <haoqunjiang@gmail.com>
2020-01-13 20:52:16 +0800

    test: set up basic testing workflow

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 module.exports = {
   extends: [
-    require.resolve('eslint-config-airbnb-base')
+    require.resolve('eslint-config-airbnb-base'),
   ],
   settings: {
     'import/resolver': {
       // https://github.com/benmosher/eslint-plugin-import/issues/1396
       [require.resolve('eslint-import-resolver-node')]: {},
       [require.resolve('eslint-import-resolver-webpack')]: {
-        config: require.resolve('@vue/cli-service/webpack.config.js')
-      }
+        config: require.resolve('@vue/cli-service/webpack.config.js'),
+      },
     },
     'import/extensions': [
       '.js',
       '.jsx',
       '.mjs',
       '.ts',
-      '.tsx'
-    ]
+      '.tsx',
+    ],
   },
   rules: {
     'import/extensions': ['error', 'always', {
@@ -24,15 +24,15 @@ module.exports = {
       mjs: 'never',
       jsx: 'never',
       ts: 'never',
-      tsx: 'never'
+      tsx: 'never',
     }],
     'no-param-reassign': ['error', {
       props: true,
       ignorePropertyModificationsFor: [
         'state', // for vuex state
         'acc', // for reduce accumulators
-        'e' // for e.returnvalue
-      ]
-    }]
-  }
-}
+        'e', // for e.returnvalue
+      ],
+    }],
+  },
+};

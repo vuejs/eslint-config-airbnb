933712235f6163e3457ef6e52c8e0247218e452e
砂糖梨子 <i@riko.love>
2023-08-10 16:38:30 +0800

    Remove no-setup-props-destructure

--- packages/eslint-config-airbnb/rules/misc.js
+++ packages/eslint-config-airbnb/rules/misc.js
@@ -14,10 +14,5 @@ module.exports = {
     // Corresponding to `react/no-typos` but a little bit different.
     // https://github.com/airbnb/javascript/blob/cbf9ade10a2f6f06c9da6dbfa25b344bee4bbef6/packages/eslint-config-airbnb/rules/react.js#L426-L428
     'vue/no-potential-component-option-typo': 'error',
-
-    // https://eslint.vuejs.org/rules/no-setup-props-destructure.html
-    // (The rule is already in `plugin:vue/essential` and `plugin:vue/vue3-essential` ruleset.)
-    // Corresponding to `react/destructuring-assignment` but the requirement in Vue is the contrary:
-    'vue/no-setup-props-destructure': 'error',
   },
 };
